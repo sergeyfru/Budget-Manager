a7984ff957f2429f402ecec628659142c334b72c
Dmitry Ashavskiy <ashavskydb@users.noreply.github.com>
2024-04-21 00:45:02 +0300

    Add front end ver.1

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 const express = require('express')
 const route = require('./routes/routes.js')
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = formPopup.querySelector(".close-btn");
+const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
 
+hamburgerBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
+});
+
+hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+
+showPopupBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show-popup");
+});
+
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+signupLoginLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
+    });
+});
 const app = express()
 
 
 app.use('/',route)
 
 
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+ arrow[i].addEventListener("click", (e)=>{
+let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+arrowParent.classList.toggle("showMenu");
+ });
+}
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+ sidebar.classList.toggle("close");
+});
 
 
 const port = process.env.PORT || 5000;
