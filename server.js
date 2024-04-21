b14fd12e7e0ae461fb4912e8c954c6d79b44af61
Dmitry Ashavskiy <ashavskydb@users.noreply.github.com>
2024-04-21 13:10:12 +0300

    add server.js

--- server.js
+++ server.js
@@ -1,52 +1,27 @@
 const express = require('express')
 const route = require('./routes/routes.js')
-const hamburgerBtn = document.querySelector(".hamburger-btn");
-const hideMenuBtn = navbarMenu.querySelector(".close-btn");
-const showPopupBtn = document.querySelector(".login-btn");
-const formPopup = document.querySelector(".form-popup");
-const hidePopupBtn = formPopup.querySelector(".close-btn");
-const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
 
-hamburgerBtn.addEventListener("click", () => {
-    navbarMenu.classList.toggle("show-menu");
-});
-
-hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
-
-showPopupBtn.addEventListener("click", () => {
-    document.body.classList.toggle("show-popup");
-});
-
-hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
-
-signupLoginLink.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
-    });
-});
 const app = express()
 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.use('/',route)
 
+app.use(express.static('./public'))
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
 
-let arrow = document.querySelectorAll(".arrow");
-for (var i = 0; i < arrow.length; i++) {
- arrow[i].addEventListener("click", (e)=>{
-let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
-arrowParent.classList.toggle("showMenu");
- });
-}
-let sidebar = document.querySelector(".sidebar");
-let sidebarBtn = document.querySelector(".bx-menu");
-console.log(sidebarBtn);
-sidebarBtn.addEventListener("click", ()=>{
- sidebar.classList.toggle("close");
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 
-const port = process.env.PORT || 5000;
+
+const port = 5000;
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.use('/',route)
