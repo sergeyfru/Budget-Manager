69ddfe0c71ca52263912b10b80e1cedf80764d01
Dmitry Ashavskiy <ashavskydb@users.noreply.github.com>
2024-05-03 22:05:47 +0300

    Update login page with new background, remove hamburger menu, and update form script
    The changes include:
    - updating the background of the login page
    - removing the hamburger menu and related code
    - updating the form

--- public/login.js
+++ public/login.js
@@ -1,33 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-const hamburgerBtn = document.querySelector(".hamburger-btn");
 const showPopupBtn = document.querySelector(".login-btn");
 const formPopup = document.querySelector(".form-popup");
 const hidePopupBtn = formPopup.querySelector(".close-btn");
-const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
-function showPopup() {
+const signupLoginLink = document.querySelectorAll(".bottom-link a");
+
+// Show login popup
+showPopupBtn.addEventListener("click", () => {
     document.body.classList.add("show-popup");
-}
-
-function hidePopup() {
-    document.body.classList.remove("show-popup");
-}
-
-
-showPopupBtn.addEventListener('click', showPopup);
-
-
-hidePopupBtn.addEventListener('click', hidePopup);
-
-
-signupLoginLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        if (link.id === 'signup-link') {
-            formPopup.classList.add('show-signup');
-        } else if (link.id === 'login-link') {
-            formPopup.classList.remove('show-signup');
-        }
-    });
+    formPopup.classList.remove("show-signup");
 });
+
+// Hide login popup
+hidePopupBtn.addEventListener("click", () => {
+    document.body.classList.remove("show-popup");
+});
+
+// Show or hide signup form
+signupLoginLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const isSignup = link.id === 'signup-link';
+        if (isSignup) {
+            formPopup.classList.add("show-signup");
+        } else {
+            formPopup.classList.remove("show-signup");
+        }
+        document.body.classList.add("show-popup");
+    });
 });
