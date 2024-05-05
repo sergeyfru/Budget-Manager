b70fda259e85297b6c5a8072500b5dae0357aab4
Dmitry Ashavskiy <ashavskydb@users.noreply.github.com>
2024-05-05 15:47:15 +0300

    update login.js

--- public/login.js
+++ public/login.js
@@ -1,29 +1,38 @@
-const showPopupBtn = document.querySelector(".login-btn");
-const formPopup = document.querySelector(".form-popup");
-const hidePopupBtn = formPopup.querySelector(".close-btn");
-const signupLoginLink = document.querySelectorAll(".bottom-link a");
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.querySelector('form');
 
-// Show login popup
-showPopupBtn.addEventListener("click", () => {
-    document.body.classList.add("show-popup");
-    formPopup.classList.remove("show-signup");
-});
+    loginForm.addEventListener('submit', async function (event) {
+        event.preventDefault(); // Prevent the default form submission
 
-// Hide login popup
-hidePopupBtn.addEventListener("click", () => {
-    document.body.classList.remove("show-popup");
-});
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const data = { username, password };
 
-// Show or hide signup form
-signupLoginLink.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        const isSignup = link.id === 'signup-link';
-        if (isSignup) {
-            formPopup.classList.add("show-signup");
-        } else {
-            formPopup.classList.remove("show-signup");
+        try {
+            // Adjust the URL to the endpoint where your server handles the login verification
+            const response = await fetch('http://localhost:3001/finance/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            const userData = await response.json();
+
+            if (response.ok) {
+                // Set user data in session storage
+                window.sessionStorage.setItem("userid", JSON.stringify(userData));
+                // Navigate to the budget.html page if the login is successful
+                window.location.href = './main2.html';
+            } else {
+                // Display an error message from the server if login is unsuccessful
+                alert(userData.message);
+            }
+        } catch (error) {
+            console.error('Error logging in:', error);
+            alert('An error occurred. Please try again later.');
         }
-        document.body.classList.add("show-popup");
     });
 });
+
